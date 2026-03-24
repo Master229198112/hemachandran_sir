748c091cbe2f0e9fe2d908ec5e1c085a863f4bd7
Vishal Kumar Sharma <vishalkumar.sharma37@gmail.com>
2026-03-24 17:56:22 +0530

    Update: Error solved

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import Link from 'next/link';
 import styles from './books.module.css';
 import dbConnect from '@/lib/mongodb';
--- src/app/research/page.tsx
+++ src/app/research/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import Link from 'next/link';
 import dbConnect from '@/lib/mongodb';
 import Publication from '@/models/Publication';
